e274c9a6f7ff1a755214561f334f86c940895723
Oluwasegun Adedeji <oluwasheges@gmail.com>
2025-10-05 17:25:01 +0100

    Update database.js

--- Backend_second_semester_exam/config/database.js
+++ Backend_second_semester_exam/config/database.js
@@ -5,9 +5,24 @@ config();
 
 const DB_URL = process.env.DB_URL;
 
-export const connectDB = () => {
-  mongoose.connect(DB_URL).then(
-    () => console.log("Database Connected"),
-    () => console.log("Error Connecting to Database")
-  );
+// export const connectDB = () => {
+//   mongoose.connect(DB_URL).then(
+//     () => console.log("Database Connected"),
+//     () => console.log("Error Connecting to Database")
+//   );
+// };
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URL, {
+      // These options are no longer needed in Mongoose 6+, but won't hurt
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
